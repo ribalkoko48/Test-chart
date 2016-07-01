bfcc75246d21c814cdf356557406d09e14887b86
kot_inc <kotara22@rambler.ru>
2016-07-01 18:11:55 +0300

    select ready not all

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-var tesdt = {}
+
+var mainList = document.querySelectorAll('.selectType');
+
+console.log(mainList)
+
+mainList.forEach(function(element_LI){
+    element_LI.addEventListener('click', periodScreen);
+})
+
+
+function periodScreen() {
+    var ulNode = this.querySelector('.prop_list');
+   
+    if (ulNode.style.display == "none") {
+        ulNode.style.display = "block";
+    }
+    else {
+        ulNode.style.display = "none";
+    }
+
+}
+
