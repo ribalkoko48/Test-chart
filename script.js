2aa051ab53166147f95820deaff2f1d09a2e6ae7
kot_inc <kotara22@rambler.ru>
2016-07-05 09:09:57 +0300

    set all eventListener for selectType

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
+//закрытие ранее отрытых выпадающих окон
+window.addEventListener('click', closeSelectTypeIfOpen);
+
+
 var mainList = document.querySelectorAll('.selectType');
 
 mainList.forEach(function (element_LI) {
     element_LI.addEventListener('click', openPropList);
 });
-
 
 //выбор периода
 var prop_listliAll = document.querySelectorAll('.prop_list li');
@@ -12,20 +15,28 @@ prop_listliAll.forEach(function (element_li) {
     element_li.addEventListener('click', setPropListValue)
 })
 
-// замена подписи на кнопке
-function setPropListValue(e) {
-    var selectedValue = this.innerHTML;
 
 
-    this.parentElement.parentElement.querySelector('span').innerHTML = selectedValue;
-    this.parentElement.classList.remove('active');
 
-    e.stopPropagation()
+
+
+//закрытие всех окон вне диапазона кнопок
+function closeSelectTypeIfOpen(e) {
+    var target = event.target;
+
+    if (!target.closest('.selectType')) {
+        closeAllPropLists()
+    }
 }
 
+function closeAllPropLists() {
+    mainList.forEach(function (item) {
+        item.querySelector('.prop_list').classList.remove('active');
+    })
+}
 
 // обработчик событий для окрытия выпадающих окон
-function openPropList() {
+function openPropList(e) {
     closeAllPropLists();
 
     // закрываем если было открыто
@@ -39,23 +50,17 @@ function openPropList() {
 
 
 }
-//закрытие ранее отрытых выпадающих окон
-window.addEventListener('click', closeSelectTypeIfOpen);
 
-//закрытие всех окон вне диапазона кнопок
-function closeSelectTypeIfOpen(event) {
-    var target = event.target;
+// замена подписи на кнопке
+function setPropListValue(e) {
+    var selectedValue = this.innerHTML;
 
-    if (!target.closest('.selectType')) {
-        closeAllPropLists()
-    }
+    this.parentElement.parentElement.querySelector('span').innerHTML = selectedValue;
+    this.parentElement.classList.remove('active');
+
+    e.stopPropagation()
 }
 
-function closeAllPropLists() {
-    mainList.forEach(function (item) {
-        item.querySelector('.prop_list').classList.remove('active');
-    })
-}
 
 
 //
